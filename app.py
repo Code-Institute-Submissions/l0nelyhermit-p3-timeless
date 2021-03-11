a7b9170a870f5c1cf0288db1669f30c3b9707262
l0nelyhermit <mrjonathangoh@gmail.com>
2021-03-11 21:31:09 +0000

    added create route

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 import os
+import pymongo
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
+
+# SET UP MONGO CONNECTION
+MONGO_URI = os.environ.get('MONGO_URI')
+DATABASE = 'Watches'
+COLLECTION = 'listing'
+# Connection to MONGODB
+connect = pymongo.MongoClient(MONGO_URI)
+db = connect[DATABASE][COLLECTION]
 
 
 @app.route('/')
 def index():
+    # data = db.find({})
     return render_template("index.html")
 
-
+@app.route('/create')
+def create():
+    return render_template("create_post.html")
+    
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
