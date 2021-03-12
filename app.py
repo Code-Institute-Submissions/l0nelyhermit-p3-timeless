185d6e3ae056442e89a90d8b269cbbfa58ee7d50
l0nelyhermit <mrjonathangoh@gmail.com>
2021-03-12 00:21:32 +0000

    link all routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 import os
 import pymongo
 from dotenv import load_dotenv
@@ -18,13 +18,30 @@ db = connect[DATABASE][COLLECTION]
 
 @app.route('/')
 def index():
-    # data = db.find({})
-    return render_template("index.html")
+    data = db.find({}).limit(3)
+    return render_template("index.html", data=data)
 
-@app.route('/create')
-def create():
+
+@app.route('/create_post')
+def create_post():
+
     return render_template("create_post.html")
-    
+
+
+@app.route('/edit_post')
+def edit_post():
+    return render_template("edit_post.html")
+
+
+@app.route('/confirm_delete_post')
+def confirm_delete_post():
+    return render_template("confirm_delete_post.html")
+
+@app.route('/delete_post')
+def delete_post():
+    return redirect(url_for('index'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
