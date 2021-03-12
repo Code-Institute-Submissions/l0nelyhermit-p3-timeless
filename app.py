2ee3b9fcd0284a65b96ce936bb91a852c6793fab
l0nelyhermit <mrjonathangoh@gmail.com>
2021-03-12 10:46:10 +0000

    styling for navbar and index.html

--- app.py
+++ app.py
@@ -27,6 +27,10 @@ def index():
     data = db.find({})
     return render_template("index.html", data=data)
 
+@app.route('/show_post')
+def show_post():
+    data = db.find({})
+    return render_template("show_post.html", data=data)
 
 @app.route('/create_post')
 def create_post():
@@ -42,10 +46,12 @@ def insert_post():
     price = request.form.get("price")
     uploadURL = request.form.get('uploaded-file-url')
     assetID = request.form.get('asset-id')
+
+    # save to database
     db.insertMany({
-        'content': content,
         'watch_brand': watch_brand,
         'watch_model': watch_model,
+        'content': content,
         'price': price,
         'uploadURL': uploadURL,
         'assetID': assetID
@@ -58,12 +64,34 @@ def edit_post(item_id):
     watch = db.find_one({
         "_id": ObjectId(item_id)
     })
-    return render_template("edit_post.html", watch=watch)
+    return render_template("edit_post.html", watch=watch,
+                           cloud_name=CLOUD_NAME, upload_preset=UPLOAD_PRESET)
 
 
-# @app.route('/edit_post', methods=['POST'])
-# def save_post():
-#     return redirect(url_for('index'))
+@app.route('/edit_post/<item_id>', methods=['POST'])
+def save_post(item_id):
+    # Load in data from form
+    watch_brand = request.form.get("watch_brand")
+    watch_model = request.form.get("watch_model")
+    content = request.form.get("content")
+    price = request.form.get("price")
+    uploadURL = request.form.get('uploaded-file-url')
+    assetID = request.form.get('asset-id')
+
+    # update Mongo database
+    db.update({
+        "_id": ObjectId(item_id)
+    }, {
+        '$set': {
+            'watch_brand': watch_brand,
+            'watch_model': watch_model,
+            'content': content,
+            'price': price,
+            'uploadURL': uploadURL,
+            'assetID': assetID
+        }
+    })
+    return redirect(url_for('index'))
 
 
 @app.route('/confirm_delete_post')
