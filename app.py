60ce6b6f41a3cefc7476ef84c1f5e62597da57ce
l0nelyhermit <mrjonathangoh@gmail.com>
2021-03-12 02:50:28 +0000

    submit form and save to database

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 import os
 import pymongo
 from dotenv import load_dotenv
@@ -26,6 +26,19 @@ def index():
 def create_post():
 
     return render_template("create_post.html")
+@app.route('/create_post', methods=['POST'])
+def insert_post():
+    watch_brand = request.form.get("watch_brand")
+    watch_model = request.form.get("watch_model")
+    content = request.form.get("content")
+    price = request.form.get("price")
+    db.insert({
+        'content': content,
+        'watch_brand': watch_brand,
+        'watch_model': watch_model,
+        'price':price
+    })
+    return redirect(url_for('index'))
 
 
 @app.route('/edit_post')
@@ -33,9 +46,16 @@ def edit_post():
     return render_template("edit_post.html")
 
 
+@app.route('/edit_post', methods=['POST'])
+def save_post():
+    
+    return redirect(url_for('index'))
+
+
 @app.route('/confirm_delete_post')
 def confirm_delete_post():
     return render_template("confirm_delete_post.html")
+
 
 @app.route('/delete_post')
 def delete_post():
